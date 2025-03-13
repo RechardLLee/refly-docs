b11f6b2f6f204f0243bebe1f5ffb323e54aad24c
pftom <1043269994@qq.com>
2025-03-13 11:54:56 +0800

    fix: Standardize string literals to use single quotes in configuration and theme files

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import { useData, inBrowser } from 'vitepress'
-import DefaultTheme from 'vitepress/theme'
-import { watchEffect } from 'vue'
+import { useData, inBrowser } from 'vitepress';
+import DefaultTheme from 'vitepress/theme';
+import { watchEffect } from 'vue';
 
-const { lang } = useData()
+const { lang } = useData();
 
 watchEffect(() => {
   if (inBrowser) {
-    document.cookie = `nf_lang=${lang.value}; expires=Mon, 1 Jan 2030 00:00:00 UTC; path=/`
+    document.cookie = `nf_lang=${lang.value}; expires=Mon, 1 Jan 2030 00:00:00 UTC; path=/`;
   }
-})
+});
 </script>
 
 <template>
